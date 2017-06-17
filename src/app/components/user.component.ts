91b4469f8c6f27ccdba8c4f8d48aedb83fa12dbd
Tarun <tarun.goenka12@gmail.com>
2017-06-17 09:58:59 +0530

    second commit

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -1,34 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'user',
-  template: `
-  
-  <p>What should i call you?</p>
-  <form>
-    <label>Name:</label>
-    <input type="text" name="name" [(ngModel)]="name"/>
-  </form>
-  <h1>Hello {{name}}!</h1>
-  <p>Do you wanna know some secrets?</p>
-  <button (click)="showmore()">{{showMore ? "Hide Secrets!":"Yes!"}} </button>
-  <div *ngIf="showMore">
-     <h5>Hahaha nothing over here!<br/>BUT<br/>Ssshhh let's keep that a Secret!</h5>
-     <p>Do you have any secrets to add?<p>
-     <ul>
-        <li *ngFor = "let secret of secrets;let i = index">
-            {{secret}} <button (click)="deletesecret(i)">X</button>
-        </li>
-     </ul>   
-        <form (submit)="addSecret(secret.value)">
-            <label>Add secrets</label>
-            <input type=text #secret/>
-        </form>
-  </div>
-  
-       
-  `,
-})
+    moduleId: module.id,
+    selector: 'user',
+    templateUrl:'user.component.html',
+    })
 export class UserComponent  {
     name:string;
     showMore:boolean;
@@ -38,7 +14,6 @@ export class UserComponent  {
      this.name='Stranger';
      this.showMore=false;
      this.secrets=[];
-    
     }
 
      showmore(){
@@ -50,12 +25,15 @@ export class UserComponent  {
          }
      }
 
-     addSecret(secret){
+     addSecret(secret:any){
          this.secrets.push(secret);
+
      }
 
-     deletesecret(i){
-         this.secrets.splice(i,1);
+     deletesecret(j:any){
+         this.secrets.splice(j,1);
      }
+
+     
 }
 
